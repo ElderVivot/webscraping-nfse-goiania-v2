d7ae7c8a1b1fa7a77f36128f5bc1b086a7750340
ElderVivot <eldervivot@gmail.com>
2022-07-24 17:38:17 -0300

    feat: implement to check if exist warning in companie

--- src/scrapings/CheckIfSelectLoaded.ts
+++ src/scrapings/CheckIfSelectLoaded.ts
@@ -5,16 +5,25 @@ import { TreatsMessageLog } from './TreatsMessageLog'
 
 export const CheckIfSelectLoaded = async (page: Page, settings: ISettingsGoiania): Promise<void> => {
     try {
+        await page.waitForTimeout(2000)
+
+        const existMessageError = await page.$('.Feedback_Message_Error')
+        if (existMessageError) throw 'EXIST_WARNING_WITH_ERRORS'
+
         await page.waitForFunction(
             `document.querySelector('#GoianiaTheme_wtTelaPrincipal_block_wtTitle_wtDadosCAE').textContent.includes(${settings.cityRegistration})`
         )
-        await page.waitForTimeout(2500) // wait 2,5 seconds to finish load of data of new select
+        await page.waitForTimeout(500) // wait 2,5 seconds to finish load of data of new select
     } catch (error) {
         settings.typeLog = 'error'
         settings.messageLog = 'CheckIfSelectLoaded'
         settings.messageError = error
         settings.messageLogToShowUser = 'Erro ao checar se a troca de empresa foi realizada.'
         settings.pathFile = __filename
+        if (error === 'EXIST_WARNING_WITH_ERRORS') {
+            settings.typeLog = 'warning'
+            settings.messageLogToShowUser = 'Empresa possui advertências/erros'
+        }
 
         const treatsMessageLog = new TreatsMessageLog(page, settings)
         await treatsMessageLog.saveLog()
