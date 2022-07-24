68a9b1dfbcb4bf39de91523437c22fe16f12d396
ElderVivot <eldervivot@gmail.com>
2022-07-24 20:19:51 -0300

    feat: implement to check if button download xml is enable

--- src/scrapings/ClickDownloadXML.ts
+++ src/scrapings/ClickDownloadXML.ts
@@ -9,8 +9,10 @@ export const ClickDownloadXML = async (page: Page, settings: ISettingsGoiania): 
         await checkIfLoadedThePage(page, 'cpo', true)
         const frame = page.frames().find(frame => frame.name() === 'cpo')
         if (frame) {
-            await frame.waitForSelector("tr td font a[href*='snfse00200f3']")
-            await frame.click("tr td font a[href*='snfse00200f3']")
+            await frame.waitForTimeout(1500)
+            const downloadIsEnable = await frame.$("tr td font a[href*='snfse00200f3']")
+            if (downloadIsEnable) await frame.click("tr td font a[href*='snfse00200f3']")
+            else throw 'BUTTON_TO_DOWNLOAD_XMLS_IS_NOT_ENABLE'
         } else {
             throw 'NOT_FOUND_FRAME_CPO'
         }
@@ -20,6 +22,10 @@ export const ClickDownloadXML = async (page: Page, settings: ISettingsGoiania): 
         settings.messageError = error
         settings.messageLogToShowUser = 'Erro ao clicar no botão "Download de XML de Notas Fiscais por período".'
         settings.pathFile = __filename
+        if (error === 'BUTTON_TO_DOWNLOAD_XMLS_IS_NOT_ENABLE') {
+            settings.typeLog = 'warning'
+            settings.messageLogToShowUser = 'Opção de fazer o download dos xmls desabilitado'
+        }
 
         const treatsMessageLog = new TreatsMessageLog(page, settings)
         await treatsMessageLog.saveLog()
